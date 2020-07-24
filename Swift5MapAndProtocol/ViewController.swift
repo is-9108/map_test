322182997228f11f4747fcf2cf2b0c49192c3a01
is-9108 <syouta103010@gmail.com>
2020-07-24 21:22:25 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol
+//
+//  Created by Shota Ishii on 2020/07/24.
+//  Copyright © 2020 is. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
